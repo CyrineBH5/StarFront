9edc591a39e6547cd3fa8e53e44de280a6565ccc
OussemaBhenni <oussemabenhenni@gmail.com>
2023-10-26 13:05:38 +0100

    add the routing with many bugss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AjouterCoursComponent } from './component/Cours/ajouter-cours/ajouter-cours.component';
+import { AjouterLeconComponent } from './component/Lecon/ajouter-lecon/ajouter-lecon.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : "addCours", component: AjouterCoursComponent},
+  {path : "addLecon", component: AjouterLeconComponent},
+  {path : "", redirectTo : "addCours", pathMatch : "full"}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
