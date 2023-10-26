9759728164be896b42c98c8be0093c7c60939eee
CyrineBH5 <148725299+CyrineBH5@users.noreply.github.com>
2023-10-26 13:06:28 +0100

    ajout design+ service

--- src/app/services/lecon/lecon.service.ts
+++ src/app/services/lecon/lecon.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Lecon } from 'src/app/models/Lecon/lecon';
 @Injectable({
   providedIn: 'root'
 })
 export class LeconService {
 
-  constructor() { }
+  constructor(private httpLecon: HttpClient) { }
+  ajouterLecon(l: Lecon) {
+    // Define your headers
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',  // Adjust the content type as needed
+      })
+    };
+
+    this.httpLecon.post<Lecon[]>('http://localhost:3003/api/Lecon', l, httpOptions).subscribe(res => {
+      console.log(res);
+    }
+      , err => { console.log(err) });
+
+  }
 }
