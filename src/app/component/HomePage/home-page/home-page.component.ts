e7d3faecedf83384bb12f5d805eeb2a340b66ba5
CyrineBH5 <148725299+CyrineBH5@users.noreply.github.com>
2023-11-16 11:53:14 +0100

    add template2

--- src/app/component/HomePage/home-page/home-page.component.ts
+++ src/app/component/HomePage/home-page/home-page.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css', '../css/style.css', '../css/bootstrap.min.css', '../lib/owlcarousel/assets/owl.carousel.min.css',
+    '../lib/animate/animate.min.css'
+
+  ]
 })
 export class HomePageComponent implements OnInit {
 
